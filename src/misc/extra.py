524bcc82d06934653203c318fde6e392ce905bda
Celestia Ludenberg <ash8820@proton.me>
2026-06-12 19:37:59 +0200

    fix regain_privileges: actually try seteuid(0) instead of giving up

--- src/misc/extra.py
+++ src/misc/extra.py
@@ -145,10 +145,9 @@ def regain_privileges():
     _DROPPED_PRIVILEGES -= 1
     if _DROPPED_PRIVILEGES == 0:
         if os.geteuid() != 0:
-            return
-        os.seteuid(0)
-        os.setegid(0)
-        os.setgroups([])
+            os.seteuid(0)
+            os.setegid(0)
+            os.setgroups([])
 
 def drop_privileges_save():
     """ Drop the real UID/GID as well, and hide them in saved IDs. """
